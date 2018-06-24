353a88f1f4533f94e26e40f57229a5029ed61f3d
William <soywaco@gmail.com>
2018-06-23 23:23:43 -0600

    Inicio del proyecto
    - Creación del proyecto Laravel.
    - Configuraciones generales realizadas.
    - Agregada la carpeta es para mensajes en español.

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DSIproject\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'DSIproject\Events\Event' => [
+            'DSIproject\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
