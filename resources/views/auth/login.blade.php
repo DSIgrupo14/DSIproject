db7b1e6b682cd3b65c51d2e0ef692214e92ec906
William <soywaco@gmail.com>
2018-06-29 17:48:13 -0600

    prueba login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.login')
+
+@section('titulo', 'CEAA | Iniciar sesión')
+
+@section('login-msg', 'Iniciar sesión')
+
+@section('formulario')
+<form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+  @csrf
+  <div class="form-group has-feedback">
+    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Correo electrónico" name="email" value="{{ old('email') }}" required>
+    @if ($errors->has('email'))
+    <span class="invalid-feedback" role="alert">{{ $errors->first('email') }}</span>
+    @endif
+      <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+  </div>
+  <div class="form-group has-feedback">
+    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Contraseña" name="password" required>
+    @if ($errors->has('password'))
+    <span class="invalid-feedback" role="alert">{{ $errors->first('password') }}</span>
+    @endif
+      <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+  </div>
+  <div class="row">
+    <div class="col-xs-8">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordar
+        </label>
+      </div>
+    </div>
+    <!-- /.col -->
+    <div class="col-xs-4">
+      <button type="submit" class="btn btn-primary btn-block btn-flat">Acceder</button>
+    </div>
+    <!-- /.col -->
+  </div>
+</form>
+
+<a href="{{ route('password.request') }}" class="text-center">Olvidé mi contraseña</a><br>
+<a href="#" class="text-center">Registrarme</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,51 +11,56 @@
 |
 */
 
-Route::get('/', function () {
-    //return view('welcome');
-    return view('layouts.general');
+Route::get('/', 'HomeController@index');
+
+Route::group(['middleware' => ['auth']], function() {
+
+    // Roles de usuario.
+    Route::resource('roles', 'RolController');
+
+    // Docentes.
+    Route::resource('docentes', 'DocenteController');
+
+    // Usuarios.
+    Route::resource('users', 'UserController');
+
+    // Materias.
+    Route::resource('materias', 'MateriaController');
+
+    //Nivel Educativo
+    Route::resource('nivel', 'NivelEducativoController');
+
+    // Grados
+    Route::resource('grados', 'GradoController');
+
+    // Años
+    Route::resource('anios','AnioController');
+
+    //Jornada laboral
+    Route::resource('jornadas','JornadaController'); 
+
+    // Vista de la Pagina Reportes
+    Route::get('reportes', function(){
+	    return view('pdf.principal');
+    })->name('reportes');
+
+    // Para descargar PDF de Grados
+    Route::get('descargar/grados', 'GradoController@pdf')->name('grados.pdf');
+
+    // Para descargar PDF de Docentes
+    Route::get('descargar/docentes', 'DocenteController@pdf')->name('docentes.pdf');
+
+    // Para descargar PDF de niveles
+    Route::get('descargar/niveles', 'NivelEducativoController@pdf')->name('nivel.pdf');
+
+    // Para descargar PDF de MAterias
+    Route::get('descargar/materias', 'MateriaController@pdf')->name('materias.pdf');
+
+    // Para descargar PDF de Jornadas Laborales
+    Route::get('descargar/jornadas', 'JornadaController@pdf')->name('jornadas.pdf');
+
 });
 
-// Roles de usuario.
-Route::resource('roles', 'RolController');
+Auth::routes();
 
-// Docentes.
-Route::resource('docentes', 'DocenteController');
-
-// Usuarios.
-Route::resource('users', 'UserController');
-
-// Materias.
-Route::resource('materias', 'MateriaController');
-
-//Nivel Educativo
-Route::resource('nivel', 'NivelEducativoController');
-
-// Grados
-Route::resource('grados', 'GradoController');
-
-// Años
-Route::resource('anios','AnioController');
-
-//Jornada laboral
-Route::resource('jornadas','JornadaController'); 
-
-// Vista de la Pagina Reportes
-Route::get('reportes', function(){
-	return view('pdf.principal');
-})->name('reportes');
-
-// Para descargar PDF de Grados
-Route::get('descargar/grados', 'GradoController@pdf')->name('grados.pdf');
-
-// Para descargar PDF de Docentes
-Route::get('descargar/docentes', 'DocenteController@pdf')->name('docentes.pdf');
-
-// Para descargar PDF de niveles
-Route::get('descargar/niveles', 'NivelEducativoController@pdf')->name('nivel.pdf');
-
-// Para descargar PDF de MAterias
-Route::get('descargar/materias', 'MateriaController@pdf')->name('materias.pdf');
-
-// Para descargar PDF de Jornadas Laborales
-Route::get('descargar/jornadas', 'JornadaController@pdf')->name('jornadas.pdf');
+Route::get('/home', 'HomeController@index')->name('home');
