5aefdd6d96f37c4f08e3892d6260538607edb9e4
William <soywaco@gmail.com>
2018-06-29 02:50:11 -0600

    Bootstrap repetido
    Por alguna razón estaba repetida la línea del llamado de los estilos de
    Bootstrap. Borre la repetida para que los estilos de AdminLTE volvieran a
    la normalidad.

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -20,7 +20,6 @@
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="{{ asset('css/skin-blue.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
