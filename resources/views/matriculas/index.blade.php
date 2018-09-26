132efe82cc706ce8db35a4917ed350dfd02e3cf3
William <soywaco@gmail.com>
2018-09-25 23:23:28 -0600

    Modificaciones en las vistas
    - Agregado el link para notas.
    - Agregado título para botones de acciones en los indexs de alumnos y
      matrículas.

--- resources/views/matriculas/index.blade.php
+++ resources/views/matriculas/index.blade.php
@@ -63,10 +63,10 @@
             @endif
             <td>
               @if ($matricula->editable == 1)
-              <a href="{{ route('matriculas.edit', $matricula->id) }}" class="btn btn-default btn-flat">
+              <a href="{{ route('matriculas.edit', $matricula->id) }}" class="btn btn-default btn-flat" title="Editar">
                 <i class="fa fa-wrench" aria-hidden="true"></i>
               </a>
-              <a href="" data-target="#modal-delete-{{ $matricula->id }}" data-toggle="modal" class="btn btn-danger btn-flat">
+              <a href="" data-target="#modal-delete-{{ $matricula->id }}" data-toggle="modal" class="btn btn-danger btn-flat" title="Eliminar">
                 <i class="fa fa-trash" aria-hidden="true"></i>
               </a>
               @endif
